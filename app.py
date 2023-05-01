55c7248dbc1e542feb5198fff93cab15649335fd
Lucas Ludwig <lucas.j.ludwig@gmail.com>
2023-05-01 16:00:00 -0400

    ready

--- app.py
+++ app.py
@@ -1,30 +1,143 @@
 # Import the dependencies.
+import numpy as np
+import datetime as dt
+import pandas as pd
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
 
 
-
-#################################################
 # Database Setup
-#################################################
+
+# Create engine using the `hawaii.sqlite` database file
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 
 # reflect an existing database into a new model
+Base = automap_base()
 
 # reflect the tables
-
+Base.prepare(engine, reflect=True)
 
 # Save references to each table
+Measurement = Base.classes.measurement
+
+Station = Base.classes.station
 
 
 # Create our session (link) from Python to the DB
+session = Session(engine)
 
 
 #################################################
 # Flask Setup
 #################################################
 
+from flask import Flask, jsonify
+
+app = Flask(__name__)
 
 
 
 #################################################
 # Flask Routes
 #################################################
+
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Welcome to the Hawaii Climate Analysis API!<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"        
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/temp/start/end<br/>"        
+    )
+
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    """Return a list of all precipitation data"""
+    # Query all precipitation data
+    results = session.query(Measurement.date, Measurement.prcp).all()
+
+    # Create a dictionary from the row data and append to a list of precipitation data
+    precipitation_data = []
+    for date, prcp in results:
+        precipitation_dict = {}
+        precipitation_dict["date"] = date
+        precipitation_dict["prcp"] = prcp
+        precipitation_data.append(precipitation_dict)
+
+    return jsonify(precipitation_data)
+
+
+@app.route("/api/v1.0/stations")
+def stations():
+    """Return a list of all stations"""
+    # Query all stations
+    results = session.query(Station.station).all()
+
+    # Convert list of tuples into normal list
+    station_data = list(np.ravel(results))
+
+    return jsonify(station_data)
+
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    """Return a list of all temperature observations"""
+    # Query all temperature observations
+    results = session.query(Measurement.date, Measurement.tobs).filter(Measurement.date >= '2016-08-23').filter(Measurement.station == 'USC00519281').all()
+
+    # Create a dictionary from the row data and append to a list of temperature observations
+    tobs_data = []
+    for date, tobs in results:
+        tobs_dict = {}
+        tobs_dict["date"] = date
+        tobs_dict["tobs"] = tobs
+        tobs_data.append(tobs_dict)
+
+    return jsonify(tobs_data)
+
+
+@app.route("/api/v1.0/temp/<start>")
+def start(start):
+    """Return a list of all temperature observations"""
+    # Query all temperature observations
+    results = session.query(func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)).filter(Measurement.date >= start).all()
+
+    # Create a dictionary from the row data and append to a list of temperature observations
+    start_data = []
+    for min, max, avg in results:
+        start_dict = {}
+        start_dict["min"] = min
+        start_dict["max"] = max
+        start_dict["avg"] = avg
+        start_data.append(start_dict)
+
+    return jsonify(start_data)
+
+
+@app.route("/api/v1.0/temp/<start>/<end>")
+def start_end(start, end):
+    """Return a list of all temperature observations"""
+    # Query all temperature observations
+    results = session.query(func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)).filter(Measurement.date >= start).filter(Measurement.date <= end).all()
+
+    # Create a dictionary from the row data and append to a list of temperature observations
+    start_end_data = []
+    for min, max, avg in results:
+        start_end_dict = {}
+        start_end_dict["min"] = min
+        start_end_dict["max"] = max
+        start_end_dict["avg"] = avg
+        start_end_data.append(start_end_dict)
+
+    return jsonify(start_end_data)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
